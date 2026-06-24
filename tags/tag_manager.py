d378e038b83c60635fbc780fab5785d872f0fe28
Shriyans406 <shriyans.s.sahoo@gmail.com>
2026-06-24 05:28:00 +0000

    Phase 11 done partially

---
+++ tags/tag_manager.py
@@ -0,0 +1,81 @@
+import sqlite3
+from pathlib import Path
+
+
+PROJECT_ROOT = (
+    Path(__file__)
+    .resolve()
+    .parent
+    .parent
+)
+
+DATABASE_FILE = (
+    PROJECT_ROOT
+    / "database"
+    / "snippets.db"
+)
+
+
+def add_tag(
+    file_id,
+    tag
+):
+
+    connection = sqlite3.connect(
+        DATABASE_FILE
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO tags
+        (
+            file_id,
+            tag
+        )
+        VALUES
+        (
+            ?,
+            ?
+        )
+        """,
+        (
+            file_id,
+            tag
+        )
+    )
+
+    connection.commit()
+
+    connection.close()
+
+    print(
+        "Tag added"
+    )
+
+
+def get_tags(
+    file_id
+):
+
+    connection = sqlite3.connect(
+        DATABASE_FILE
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        SELECT tag
+        FROM tags
+        WHERE file_id = ?
+        """,
+        (file_id,)
+    )
+
+    tags = cursor.fetchall()
+
+    connection.close()
+
+    return tags
